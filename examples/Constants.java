7fac2f68cad270eed7f731190c25673ba32563f7
kareiku <xaxvu7@gmail.com>
2026-09-22 00:11:18 +0200

    Replace old examples by new ones
    TODO finish these examples
    
    TODO add more examples

---
+++ examples/Constants.java
@@ -0,0 +1,4 @@
+public class Constants {
+    public static void main(String[] args) {
+    }
+}
---
+++ examples/Variables.java
@@ -0,0 +1,13 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        int number = 3;
+        char character = 'A';
+        double realNumber = 1.7;
+
+        System.out.printf("number: %d\ncharacter: %c\nrealNumber: %f\n\n",
+            number,
+            character,
+            realNumber
+        );
+    }
+}
